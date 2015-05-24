191d9894fec012f065654443db7a00ae88786e04
Grant Kurtz <kurtz.grant@gmail.com>
2015-05-24 20:37:16 +0000

    Now saves results to a txt file, and retrieves them. Also broke up the single function a bit for readability.

--- upvotehandler.py
+++ upvotehandler.py
@@ -7,41 +7,72 @@ class UpvoteHandler:
 		self.client = sc
 		self.users = usersList
 		self.logger = logging.getLogger("UpvoteHandler")
+		self.RESULT_FILE = "tallies.txt"
+		self.retrieveSavedData()
 
 	def handleEvent(self, event):
 		
+		if self.canHandle(event):
+			self.logger.debug("Plus one!")
+			channel = event['channel']
+			text = event['text']
+			user = self.parseUser(text)
+			if user is None:
+				self.logger.debug("No user found for ++")
+				return
+				
+			self.users[user] = self.users[user] + 1
+			message = "{} + {}".format(user, self.users[user])
+			self.writeCurrentResults()
+			self.logger.info(message)
+			self.client.rtm_send_message(channel, message)
+	
+	def canHandle(self, event):
 		if "type" not in event:
 			self.logger.debug("Unknown message type, ignoring")
-			return
+			return False
 		
 		if event['type'] == "message":
 			self.logger.debug("Found message")
 			if 'channel' not in event:
 				self.logger.debug("Not in a channel")
-				return
-			channel = event['channel']
+				return False
 			if "text" not in event:
 				self.logger.debug("No text object? Skipping...")
-				return
+				return False
 			text = event['text']
 			if "++" in text:
-				self.logger.debug("Plus one!")
-				if text.index("++") != 0:
-					self.logger.debug("Only support prefix notation!")
-					return
-				
-				user = text[text.index("++")+2:]
-				if user == "":
-					self.logger.debug("User's name is empty???")
-					return
-				user = user.lower()
-				self.logger.debug("User '{}'".format(user))
-				
-				if user not in self.users:
-					self.logger.debug("Cannot find user, ignoring")
-					return
-					
-				self.users[user] = self.users[user] + 1
-				message = "{} + {}".format(user, self.users[user])
-				self.logger.info(message)
-				self.client.rtm_send_message(channel, message)
+				return True
+		return False
+		
+	def parseUser(self, text):
+		plusIndex = text.index("++")
+		user = text[plusIndex+2:]
+		if user == "":
+			self.logger.debug("User's name is empty???")
+			return None
+		user = user.lower()
+		self.logger.debug("User '{}'".format(user))
+		
+		if user not in self.users:
+			self.logger.debug("Cannot find user, ignoring")
+			return None
+		return user
+			
+	def writeCurrentResults(self):
+		resultStr = ""
+		for user, count in self.users.iteritems():
+			if count == 0:
+				continue
+			resultStr = "{}\n{},{}".format(resultStr, user, count)
+		save = open(self.RESULT_FILE, "w")
+		save.write(resultStr)
+		save.close()
+		
+	def retrieveSavedData(self):
+		saved = open(self.RESULT_FILE, "r")
+		for line in saved:
+			data = line.split(",")
+			self.users[data[0]] = int(data[1])
+		saved.close()
+			
